b9e9886b261593311250583466436d63a2f9d384
”hatabe_tomoya” <arkelle1559@gmail.com>
2019-12-20 10:02:17 +0000

    ユーザー詳細途中

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -64,59 +64,39 @@
         </div>
         @if (isset($timelines))
             @foreach ($timelines as $timeline)
-                <div class="col-md-8 mb-3">
-                    <div class="card">
-                        <div class="card-haeder p-3 w-100 d-flex">
+                <div class ="study-card p-0 col-lg-4 col-md-6 col-sm-12">
+                    <div class ="card m-3">
+                        <div class ="card-header">
                             <img src="{{ $user->icon_image }}" class="rounded-circle" width="50" height="50">
                             <div class="ml-2 d-flex flex-column flex-grow-1">
                                 <p class="mb-0">{{ $timeline->user->name }}</p>
-                                <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">{{ $timeline->user->screen_name }}</a>
+                                
                             </div>
-                            <div class="d-flex justify-content-end flex-grow-1">
-                                <p class="mb-0 text-secondary">{{ $timeline->created_at->format('Y-m-d H:i') }}</p>
-                            </div>
+                            
                         </div>
                         <div class="card-body">
-                            {{ $timeline->title }}
+                            {{ $timeline->body }}
                         </div>
                         <div class="card-footer py-1 d-flex justify-content-end bg-white">
-                            @if ($timeline->user->id === Auth::user()->id)
-                                <div class="dropdown mr-3 d-flex align-items-center">
-                                    <a href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fas fa-ellipsis-v fa-fw"></i>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                        <form method="POST" action="{{ url('posts/' .$timeline->id) }}" class="mb-0">
-                                            @csrf
-                                            @method('DELETE')
-
-                                            <a href="{{ url('posts/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
-                                            <button type="submit" class="dropdown-item del-btn">削除</button>
-                                        </form>
+                            <div class ="user">
+                                    <p class ="float-left">
+                                        <a href ="#">
+                                            <img src ="#">
+                                        </a>
+                                        <p class="mb-0">{{ $timeline->user->name }}</p>
+                                    </p>
+                                </div>
+                                <div class ="like">
+                                    <div class="float-right">
+                                        <div class="d-flex align-items-center">
+                                            <i class="fas fa-heart fa-fw text-danger"></i>
+                                                <div class="mb-0 text-secondary">{{ count($timeline->like) }}</div>
+                                        </div>
                                     </div>
                                 </div>
-                            @endif
-                            <div class="mr-3 d-flex align-items-center">
-                                <a href="{{ url('posts/' .$timeline->id) }}"><i class="far fa-comment fa-fw"></i></a>
-                                <p class="mb-0 text-secondary">{{ count($timeline->comment) }}</p>
-                            </div>
+                           
                             <div class="d-flex align-items-center">
-                                @if (!in_array(Auth::user()->id, array_column($timeline->like->toArray(), 'user_id'), TRUE))
-                                    <form method="POST" action="{{ url('likes/') }}" class="mb-0">
-                                        @csrf
-
-                                        <input type="hidden" name="post_id" value="{{ $timeline->id }}">
-                                        <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
-                                    </form>
-                                @else
-                                    <form method="POST"action="{{ url('likes/' .array_column($timeline->like->toArray(), 'id', 'user_id')[Auth::user()->id]) }}" class="mb-0">
-                                        @csrf
-                                        @method('DELETE')
-
-                                        <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
-                                    </form>
-                                @endif
-                                <p class="mb-0 text-secondary">{{ count($timeline->like) }}</p>
+                                
                             </div>
                         </div>
                     </div>
